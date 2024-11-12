7d6098cffd4d877990d8c7d2e766bf499ac92c9a
Nicolas <haubrichtnicolas@gmail.com>
2024-11-12 19:19:50 -0300

    Alterando Nome do Projeto Footer e Botao

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <footer className='w-full text-center p-3 flex items-center justify-center flex-wrap bg-off-white'>
-                <p className='mr-1 text-green-1'>&copy; {new Date().getFullYear()} Copyright:{' '}</p>
-                <p className='text-dark text-green-1'>
+            <footer className='w-full text-center p-3 flex items-center justify-center flex-wrap bg-green-2'>
+                <p className='mr-2 text-off-white'>&copy; {new Date().getFullYear()} Copyright:{' '}</p>
+                <p className='text-dark text-off-white'>
                     Nicolas Haubricht Hainfellner
                 </p>
             </footer>
