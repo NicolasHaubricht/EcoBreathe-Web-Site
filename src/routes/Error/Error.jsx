9eb435603b4670d267a1ce5e0958750659ef793c
Nicolas <haubrichtnicolas@gmail.com>
2024-12-06 10:40:23 -0300

    Estilizando Error

--- src/routes/Error/Error.jsx
+++ src/routes/Error/Error.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import ButtonHome from '../../components/ButtonHome/ButtonHome'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
     return (
-        <div>Error</div>
+        <>
+            <section className='min-h-dvh flex items-center justify-center'>
+                <div className='flex items-center justify-center gap-5 flex-wrap p-8'>
+                    <h1 className='text-4xl lg:text-6xl font-semibold uppercase text-green-1'>404 Error</h1>
+                    <Link to="/">
+                        <ButtonHome text_btn="Home"></ButtonHome>
+                    </Link>
+                </div>
+            </section>
+        </>
     )
 }
 
